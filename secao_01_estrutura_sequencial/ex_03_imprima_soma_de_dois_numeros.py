c4ebe4c0c3917e1e8e420aa19fc77d9494fce45e
matheuspdf <96511087+matheuspdf@users.noreply.github.com>
2023-06-05 21:08:41 -0300

    resp ex 03

--- secao_01_estrutura_sequencial/ex_03_imprima_soma_de_dois_numeros.py
+++ secao_01_estrutura_sequencial/ex_03_imprima_soma_de_dois_numeros.py
@@ -5,13 +5,16 @@ https://wiki.python.org.br/EstruturaSequencial
 Faça um Programa que peça dois números inteiros e imprima a soma.
 
     >>> from secao_01_estrutura_sequencial import ex_03_imprima_soma_de_dois_numeros
-    >>> numeros =['42', '43']
+    >>> numeros = ['44', '43']
     >>> ex_03_imprima_soma_de_dois_numeros.input = lambda k: numeros.pop()
     >>> ex_03_imprima_soma_de_dois_numeros.imprima_a_soma_de_dois_numeros()
-    A soma dos dois números informados é 85
+    A soma dos dois números informados é 87
 
 """
 
 
 def imprima_a_soma_de_dois_numeros():
-    """Escreva aqui em baixo a sua solução"""
+    n1 = int(input("Digite o primeiro número: "))
+    n2 = int(input("Digite o segundo número: "))
+    soma = n1 + n2
+    print('A soma dos dois números informados é', soma)
