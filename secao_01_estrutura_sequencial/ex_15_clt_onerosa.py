28f828b94172e0df7bd6c1027cf887b1dc482b25
matheuspdf <96511087+matheuspdf@users.noreply.github.com>
2023-06-06 21:16:51 -0300

    resp ex 15

--- secao_01_estrutura_sequencial/ex_15_clt_onerosa.py
+++ secao_01_estrutura_sequencial/ex_15_clt_onerosa.py
@@ -19,11 +19,22 @@ Mostrar os resultados com duas casas decimais
     + Salário Bruto : 4449.60
     - IR (11%) : R$ 489.46
     - INSS (8%) : R$ 355.97
-    - Sindicato ( 5%) : R$ 222.48
-    = Salário Liquido : R$ 3381.70
+    - Sindicato (5%) : R$ 222.48
+    = Salário Líquido : R$ 3381.70
 
 """
 
 
 def calcular_assalto_no_salario():
-    """Escreva aqui em baixo a sua solução"""
+    ganho_por_hora = float(input('Quanto você ganha por hora? '))
+    horas_trabalhadas_no_mes = float(input('Quantas hora você trabalha por mês? '))
+    salario_bruto = ganho_por_hora * horas_trabalhadas_no_mes
+    imposto_de_renda = salario_bruto * 0.11
+    inss = salario_bruto * 0.08
+    sindicato = salario_bruto * 0.05
+    salario_liquido = salario_bruto - imposto_de_renda - inss - sindicato
+    print(f'+ Salário Bruto : {salario_bruto:.2f}')
+    print(f'- IR (11%) : R$ {imposto_de_renda:.2f}')
+    print(f'- INSS (8%) : R$ {inss:.2f}')
+    print(f'- Sindicato (5%) : R$ {sindicato:.2f}')
+    print(f'= Salário Líquido : R$ {salario_liquido:.2f}')
