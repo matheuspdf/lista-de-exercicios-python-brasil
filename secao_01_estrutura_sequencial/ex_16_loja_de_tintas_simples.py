0612f67d144816fbc3708af058eec6708cd7d125
matheuspdf <matheuslopes.pdf@gmail.com>
2023-06-10 21:48:34 -0300

    resp ex 16

--- secao_01_estrutura_sequencial/ex_16_loja_de_tintas_simples.py
+++ secao_01_estrutura_sequencial/ex_16_loja_de_tintas_simples.py
@@ -10,14 +10,18 @@ Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e 
     >>> from secao_01_estrutura_sequencial import ex_16_loja_de_tintas_simples
     >>> ex_16_loja_de_tintas_simples.input = lambda k: '50'
     >>> ex_16_loja_de_tintas_simples.calcular_latas_e_preco_de_tinta()
-    Você deve comprar 1 lata(s) tinta ao custo de R$ 80.00
+    Você deve comprar 1 lata(s) de tinta ao custo de R$ 80.00
     >>> ex_16_loja_de_tintas_simples.input = lambda k: '100'
     >>> ex_16_loja_de_tintas_simples.calcular_latas_e_preco_de_tinta()
-    Você deve comprar 2 lata(s) tinta ao custo de R$ 160.00
+    Você deve comprar 2 lata(s) de tinta ao custo de R$ 160.00
 
 
 """
 
 
 def calcular_latas_e_preco_de_tinta():
-    """Escreva aqui em baixo a sua solução"""
+    area = float(input('Digite o tamanho em metros quadrados da área a ser pintada: '))
+    litros_de_tinta = area / 3
+    qtd_de_latas = round(litros_de_tinta / 18)
+    custo_por_lata = qtd_de_latas * 80
+    print(f'Você deve comprar {qtd_de_latas} lata(s) de tinta ao custo de R$ {custo_por_lata}.00')
