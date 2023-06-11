43a4007999e4db808c265b7ae6d4c1c8f028101e
matheuspdf <matheuslopes.pdf@gmail.com>
2023-06-10 22:10:08 -0300

    resp ex 01 seção 02

--- secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
+++ secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
@@ -16,4 +16,9 @@ Faça um Programa que peça dois números e imprima o maior deles.
 
 
 def maior_de_dois_numeros(x, y):
-    """Escreva aqui em baixo a sua solução"""
+    if x > y:
+        print(x)
+    elif y > x:
+        print(y)
+    else:
+        print('Os números são iguais')
