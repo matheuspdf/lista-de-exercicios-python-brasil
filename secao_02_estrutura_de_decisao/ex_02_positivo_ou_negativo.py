04f4af9173d2a93a2edb3704cae38718acf5cf8e
matheuspdf <matheuslopes.pdf@gmail.com>
2023-06-20 16:52:16 -0300

    resp ex 02 seção 02

--- secao_02_estrutura_de_decisao/ex_02_positivo_ou_negativo.py
+++ secao_02_estrutura_de_decisao/ex_02_positivo_ou_negativo.py
@@ -2,7 +2,8 @@
 Exercício 02 da seção de estrutura de decisão da Python Brasil:
 https://wiki.python.org.br/EstruturaDeDecisao
 
-Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo. Caso seja igual a 0, retorne None.
+Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+Caso seja igual a 0, retorne None.
 
     >>> positivo_ou_negativo(1)
     'positivo'
@@ -16,4 +17,10 @@ Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou 
 
 
 def positivo_ou_negativo(n):
-    """Escreva aqui em baixo a sua solução"""
+    n = input('Digite um número positivo ou negativo')
+    if n > 0:
+        print('positivo')
+    elif n < 0:
+        print('negativo')
+    else:
+        print('não tem positivo nem negativo')
