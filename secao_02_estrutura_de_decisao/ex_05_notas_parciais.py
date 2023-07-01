cb37294e9cfd522bfc0351a81a6e375ec5057087
matheuspdf <matheuslopes.pdf@gmail.com>
2023-06-30 22:33:38 -0300

    resp ex 05 seção 02

--- secao_02_estrutura_de_decisao/ex_05_notas_parciais.py
+++ secao_02_estrutura_de_decisao/ex_05_notas_parciais.py
@@ -2,7 +2,9 @@
 Exercício 05 da seção de estrutura de decisão da Python Brasil:
 https://wiki.python.org.br/EstruturaDeDecisao
 
-Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+Faça um programa para a leitura de duas notas parciais de um aluno.
+O programa deve calcular a média alcançada por aluno e apresentar:
+
  - A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
  - A mensagem "Reprovado", se a média for menor do que sete;
  - A mensagem "Aprovado com Distinção", se a média for igual a dez.
@@ -20,4 +22,10 @@ Obs: 0 <= nota <= 10
 
 
 def notas_parciais(nota_1, nota_2):
-    """Escreva aqui em baixo a sua solução"""
+    notas_parciais = (nota_1 + nota_2) / 2
+    if notas_parciais == 10:
+        return 'Aprovado com Distinção'
+    elif notas_parciais >= 7:
+        return 'Aprovado'
+    else:
+        return 'Reprovado'
